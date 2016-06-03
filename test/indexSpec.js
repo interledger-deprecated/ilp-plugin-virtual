33015313290f082ee134569f073fe5c601545444
Ben Sharafian <benms64@gmail.com>
2016-06-03 12:31:14 -0700

    test: break the transaction flow into more tests

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -25,7 +25,26 @@ describe('PluginVirtual', function () {
 
   var pv1 = new PluginVirtual({store: s1store, auth: {account: "plugin 1"}, limit: 300})
   var pv2 = new PluginVirtual({store: s2store, auth: {account: "plugin 2"}, limit: 300})
+
+  it('should construct non-null objects', () => {
+    assert(pv1 && pv2)
+  })
   
+  var pv1b, pv2b
+
+  pv1.on('_balanceChanged', () => {
+    pv1.getBalance().then((balance) => {
+      pv1b = balance | 0
+      pv1._log(balance)
+    })
+  })
+  pv2.on('_balanceChanged', () => {
+    pv2.getBalance().then((balance) => {
+      pv2b = balance | 0
+      pv2._log(balance)
+    })
+  })
+
   it('should create a connect event on connect', (done) => {
 
     pv1.on('connect', () => { done() })
@@ -33,30 +52,32 @@ describe('PluginVirtual', function () {
     pv2.connect()
   })
 
-  it('should go through the proper channels for a transaction', (done) => {
-
-    // TODO: make this async once the connection class needs that
-
-    pv1.send({
-      id: 'onehundred',
-      account: 'doesnt really matter',
-      amount: '100',
+  var p = pv1.send({
+    id: 'onehundred',
+    account: 'doesnt really matter',
+    amount: '100',
+    data: new Buffer('')
+  }).then(() => {
+    return pv2.send({
+      id: 'twohundred',
+      account: 'doesnt really matter here either',
+      amount: '200',
       data: new Buffer('')
-    }).then(() => {
-      return pv2.send({
-        id: 'twohundred',
-        account: 'doesnt really matter here either',
-        amount: '200',
-        data: new Buffer('')
-      })
-    }).then(() => {
-      return pv2.send({
-        id: 'rejectthis',
-        account: 'this should get rejected',
-        amount: '400',
-        data: new Buffer('')
-      })
-    }).then(() => {
+    })
+  }).then(() => {
+    return pv2.send({
+      id: 'rejectthis',
+      account: 'this should get rejected',
+      amount: '400',
+      data: new Buffer('')
+    })
+  })
+
+  it('should reject invalid acknowledgements', (done) => {
+    pv1.on('error', () => {
+      done() 
+    })
+    p.then(() => {
       return pv2._acceptTransfer(new Transfer({
         id: 'thisdoesntexist',
         account: 'this should get rejected',
@@ -64,25 +85,13 @@ describe('PluginVirtual', function () {
         data: new Buffer('')
       }))
     })
+  })
 
-    var pv1b, pv2b
-
-    pv1.on('_balanceChanged', () => {
-      pv1.getBalance().then((balance) => {
-        pv1b = balance | 0
-        pv1._log(balance)
-      })
-    })
-    pv2.on('_balanceChanged', () => {
-      pv2.getBalance().then((balance) => {
-        pv2b = balance | 0
-        pv2._log(balance)
-      })
-    })
+  it('should finish with the correct balances', (done) => {
 
     setTimeout(() => {
       assert(pv1b === 100 && pv2b === -100, 'balances should be correct')
       done()
-    }, 250)
+    }, 100)
   })
 })

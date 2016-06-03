d08a9603b9d1aad58292919c693c03b76b27c0d0
Ben Sharafian <benms64@gmail.com>
2016-06-03 11:39:35 -0700

    feat: track all transfers in a transferlog object

---
+++ src/model/transferlog.js
@@ -0,0 +1,40 @@
+const log = require('../controllers/log')
+
+class TransferLog {
+  
+  constructor (store) {
+    this._get = store.get
+    this._put = store.put
+    this._del = store.del
+  }
+
+  getId (transferId) {
+    return this._get('t' + transferId).then((jsonTransfer) => {
+      if (jsonTransfer) {
+        return Promise.resolve(JSON.parse(jsonTransfer))
+      } else {
+        return Promise.resolve(undefined)
+      }
+    })
+  }
+
+  get (transfer) {
+    return this.getId(transfer.id)
+  }
+
+  store (transfer) {
+    return (this._put('t' + transfer.id, JSON.stringify(transfer)))
+  }
+  
+  exists (transfer) {
+    return this.get(transfer).then((storedTransfer) => {
+      return Promise.resolve(storedTransfer !== undefined)
+    })
+  }
+
+  del (transfer) {
+    return this._del('t' + transfer.id);
+  } 
+}
+
+exports.TransferLog = TransferLog

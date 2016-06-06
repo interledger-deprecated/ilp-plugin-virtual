3b305455f06d6b47f481a4e6af4e32a8439b7f87
Ben Sharafian <benms64@gmail.com>
2016-06-06 13:15:20 -0700

    refactor: switch the connection back to wrtc module
    the wrtc module installs fine so long as deps are installed and Xcode 6.4 (not 7) is used

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,138 +1,46 @@
 const EventEmitter = require('events')
-const log = require('../controllers/log')
-const webrtc = require('webrtc-native')
+// const log = require('../controllers/log')
+const SimplePeer = require('simple-peer')
 
-/* uses the webrtc package, webrtc-native. does not yet have signaling */
+/* uses the webrtc package, simple-peer. does not yet have signaling */
 
 class Connection extends EventEmitter {
 
-  /*
-    config = {
-      initiator: bool
-    }
-  */
   constructor (config) {
     super()
 
-    var rtcConfig = {
-      iceServers: [
-        { url: 'stun:stun.l.google.com:19302' }
-      ]
-    }
-    var datachannelConstraints = {
-      audio: false,
-      video: false,
-      optional: [
-        {
-          RtpDataChannels: true,
-          DlsSrtpKeyAgreement: false
-        }
-      ],
-      mandatory: {
-        OfferToReceiveAudio: false,
-        OfferToReceiveVideo: false
-      } 
-    }
-
-    this.datachannelConfig = {
-      reliable: true,
-      ordered: true
-    }
+    /*
+      config = {
+        initiator: bool
+      }
+    */
+    this.config = config
     this.initiator = config.initiator
-    this.peer = new webrtc.RTCPeerConnection(rtcConfig, datachannelConstraints)
+    this.peer = null
     this.other = null
   }
 
-  connectPeer(peerInfo) {
-    // for now just connect within one instance of node
-
-    this._log("received other")
-    this.other = peerInfo.peer
-    this.otherConnection = peerInfo
+  connectPeer (peer) {
+    this.other  = peer
   }
 
-  connect () { 
-    return new Promise((resolve) => {
-    
-      this._log("running this, at least")
-
-      var has_candidate = false
-      this.peer.onicecandidate = (event) => {
-
-        this._log("ice candidate") 
-
-        if (!has_candidate) {
-          var candidate = event.candidate || event // used in webrtc tests
-          this.other.addIceCandidate(candidate)
-          has_candidate = true
-        }
-      }
-      
-      // TODO: change to promises
-      this.peer.onnegotiationneeded = () => {      
-        this.peer.createOffer((sdp) => {        
-          this.peer.setLocalDescription(sdp, () => {
-            this.other.setRemoteDescription(sdp, () => { 
-              this.other.createAnswer((sdp) => {
-                this.other.setLocalDescription(sdp, () => {
-                  this.peer.setRemoteDescription(sdp, () => {
-                    this.emit('connect')       
-                    this.otherConnection.emit('connect')       
-                  });
-                });
-              });
-            });  
-          });
-        });
-      };
-      this.on('connect', () => {this._log('connected!')})
-
-      this.peer.ondatachannel = (event) => {
-
-        var channel = event.channel || event;
-        if (!channel) {
-          return false 
-        }
-        this._channel = channel
-        this._log("Connected to datachannel")      
-    
-        channel.onopen = () => {
-          this.emit('open') 
-        }      
-
-        channel.onmessage = (event) => {
-          this._log(event.data)
-          this.emit('receive', event.data)
-        }
-
-        channel.onclose = () => {
-          this.emit('close')
-        }
-
-        this._log("sent out probe")
-        this.emit('bloop')
-      }
-
-//      this.on('connect', () => {
-        this.on('bloop', () => {console.log('bloop') ; this._channel.send('message!')})
-        if (this.initiator) {
-          this.peer.ondatachannel(
-            this.peer.createDataChannel('connection', this.dataChannelConfig)
-          )
-        }
-//      })
-
-      resolve()
+  connect () {
+    this.peer = SimplePeer({initiator: this.initiator})
+    this.peer.on('signal', (data) => {
+      this.other.signal(data)
+    })
+    this.peer.on('data', (data) => {
+      this.emit('receive', data)
     })
   }
 
   disconnect () {
-    this._channel.close()
-    this.peer.close.close()
+    // TODO: make this thing disconnect sometime
+    return Promise.resolve(null)
   }
 
   send (msg) {
-    this._channel.send(msg)
+    this.peer.send(msg)
   }
 
   _getPeer () {
@@ -143,11 +51,6 @@ class Connection extends EventEmitter {
     return
     // TODO: remove this entirely
   }
-    
-  _log (msg) {
-    var name = this.initiator? "sender" : "recver";
-    log.log(name + ": " + msg)
-  } 
 }
 
 exports.Connection = Connection

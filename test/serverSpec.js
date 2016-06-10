bfe45a6c1e0c961198e05ce60c93bd6359b1a4d4
Ben Sharafian <benms64@gmail.com>
2016-06-10 14:22:31 -0700

    test: add tests for signalling server error cases

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -5,7 +5,7 @@ const server = require('../src/signalling/server')
 const log = require('../src/util/log')('test')
 const socketIoClient = require('socket.io-client')
 
-describe('Signalling Server', function (doneDescribe) {
+describe('Signalling Server', function () {
   it('should be an object containing the `run` function', () => {
     assert.isObject(server)
     assert.isFunction(server.run)
@@ -46,13 +46,61 @@ describe('Signalling Server', function (doneDescribe) {
     })
   })
 
-  // let tworemain = null
+  let error1 = null
+  it('should give an error on a premature answer', (done) => {
+    error1 = connected.then(() => {
+      return new Promise((resolve) => {
+        sock1.once('exception', (err) => {
+          assert.isObject(err)
+          assert.isString(err.msg)
+          log.error(err)
+          done()
+          resolve()
+        })
+        sock1.emit('respondWithAnswer', {room: 'a', answer: 'answer'})
+      })
+    })
+  })
+
+  let tworemain = null
   it('should not allow 3 sockets to a room', (done) => {
-    connected.then(() => {
+    tworemain = error1.then(() => {
       // because we're not using webrtc here the offers can be anything
       sock1.emit('startWithOffer', {room: 'a', offer: 'offer'})
       sock2.emit('startWithOffer', {room: 'a', offer: 'offer'})
       sock3.emit('startWithOffer', {room: 'a', offer: 'offer'})
+    }).then(() => {
+      return Promise.all([
+        new Promise((resolve) => {
+          let handle = (err) => {
+            assert.isObject(err)
+            assert.isString(err.msg)
+            log.error(err)
+            resolve()
+          }
+          // the errors could appear in any socket
+          sock1.once('exception', (err) => { sock1 = sock3; handle(err) })
+          sock2.once('exception', (err) => { sock2 = sock3; handle(err) })
+          sock3.once('exception', handle)
+        }),
+        new Promise((resolve) => {
+          let handle = (offer) => {
+            resolve()
+          }
+          sock1.once('offer', handle)
+          sock2.once('offer', handle)
+          sock3.once('offer', handle)
+        })
+      ]).then(() => {
+        done()
+      })
+    })
+  })
+
+  it('should give an exception if offerer sends an answer', (done) => {
+    tworemain.then(() => {
+      sock1.emit('respondWithAnswer', {room: 'a', answer: 'offer'})
+      sock2.emit('respondWithAnswer', {room: 'a', answer: 'offer'})
     }).then(() => {
       return new Promise((resolve) => {
         let handle = (err) => {
@@ -60,34 +108,11 @@ describe('Signalling Server', function (doneDescribe) {
           assert.isString(err.msg)
           log.error(err)
           done()
-          doneDescribe()
           resolve()
         }
-        // the errors could appear in any socket
-        sock1.once('exception', (err) => { sock1 = sock3; handle(err) })
-        sock2.once('exception', (err) => { sock2 = sock3; handle(err) })
-        sock3.once('exception', handle)
+        sock1.once('exception', handle)
+        sock2.once('exception', handle)
       })
     })
   })
-
-  /* WIP
-  it('should give an exception on a premature answer', (done) => {
-    tworemain.then(() => {
-      return new Promise((resolve) => {
-        let offerAndError = (s1, s2) => {
-          s1.once('offer', () => {
-            s2.emit('respondWithAnswer', {room: 'a', answer: 'answer'})
-            s2.once('exception', (err) => {
-              done()
-              doneDescribe()
-            })
-          })
-        }
-        offerAndError(sock1, sock2)
-        offerAndError(sock2, sock1)
-      })
-    })
-  })
-  */
 })

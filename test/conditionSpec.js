be150450fb49b09aa0d2ba20e08b9b2b1dac2855
Ben Sharafian <benms64@gmail.com>
2016-06-14 17:09:05 -0700

    test: test out ATP/UTP transfers between nerd and noob

--- test/conditionSpec.js
+++ test/conditionSpec.js
@@ -69,6 +69,93 @@ describe('UTP/ATP Transactions with Nerd and Noob', function () {
     })
   })
 
+  it('should not count escrowed money in balance yet', (done) => {
+    next = next.then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '0')
+      done()
+    }).catch(handle)
+  })
+
+  it('should fulfill a UTP transaction as the noob', (done) => {
+    next = next.then(() => {
+      return noob.fulfillCondition('first', fulfillment)
+    }).then(() => {
+      return new Promise((resolve) => {
+        noob.once('fulfill_execution_condition', (transfer, fulfillment) => {
+          assert(transfer.id === 'first')
+          resolve()
+        })
+      })
+    }).then(() => {
+      done()
+    })
+  })
+
+  it('should have the correct balance after executing', (done) => {
+    next = next.then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '100')
+      done()
+    }).catch(handle)
+  })
+
+  it('should submit an ATP transaction as the noob', (done) => {
+    next = next.then(() => {
+      return noob.send({
+        id: 'second',
+        account: 'x',
+        amount: '200',
+        executionCondition: 'garbage',
+        cancellationCondition: condition
+      })
+    }).then(() => {
+      return new Promise((resolve) => {
+        noob.once('accept', (transfer, message) => {
+          assert(transfer.id === 'second') 
+          resolve()
+        })
+      })
+    }).then(() => {
+      done()
+    })
+  })
+
+  it('should take escrowed balance out', (done) => {
+    next = next.then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '-100')
+      done()
+    }).catch(handle)
+  })
+
+  it('should cancel an ATP transaction as the noob', (done) => {
+    next = next.then(() => {
+      return noob.fulfillCondition('second', fulfillment)
+    }).then(() => {
+      return new Promise((resolve) => {
+        noob.once('fulfill_cancellation_condition', (transfer, fulfillment) => {
+          assert(transfer.id === 'second')
+          resolve()
+        })
+      })
+    }).then(() => {
+      done()
+    })
+  })
+
+  it('should give back escrowed funds after cancellation', (done) => {
+    next = next.then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '100')
+      done()
+    }).catch(handle)
+  })
+
   it('should disconnect gracefully', (done) => {
     next.then(() => {
       noob.disconnect()

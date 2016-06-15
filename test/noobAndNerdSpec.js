e7539bfb21e55b7382f8b0d174cc8ed46f78c9a9
Ben Sharafian <benms64@gmail.com>
2016-06-15 14:13:41 -0700

    test: add tests for the case with more than one nerd

--- test/noobAndNerdSpec.js
+++ test/noobAndNerdSpec.js
@@ -6,6 +6,7 @@ const newObjStore = require('../src/model/objStore')
 const log = require('../src/util/log')('test')
 
 let nerd = null
+let nerd2 = null
 let noob = null
 let noob2 = null
 let handle = (err) => { log.log(err) }
@@ -181,17 +182,140 @@ describe('The Noob and the Nerd', function () {
             assert(transfer.id === 'fourth')
             resolve()
           })
+        }),
+        new Promise((resolve) => {
+          nerd.once('accept', (transfer, message) => {
+            assert(transfer.id === 'fourth')
+            resolve()
+          })
         })
       ])
     }).then(() => {
       done()
     }).catch(handle)
   })
-  
+
+  it('should instantiate a second nerd', (done) => {
+    next = next.then(() => {
+      // objStore needs to be cloned manually because there isn't
+      // a standard way to clone any store
+      let objStore2 = nerd.store.clone()
+
+      nerd2 = new PluginVirtual({
+        store: objStore2,
+        auth: {
+          host: 'mqatt://test.mosquitto.org',
+          token: 'aW50ZXJsZWdlcgo',
+          limit: '1000',
+          max: '1000',
+          account: 'nerd2',
+          secret: 'secret'
+        }
+      })
+
+      // hacky way to make sure the balance doesn't start at zero
+      nerd2.balance._initialized = true
+
+      assert.isObject(nerd2)
+    }).then(() => {
+      return nerd2.connect()
+    }).then(() => {
+      done()
+    }).catch(handle)
+  })
+
+  it('should hold hold the same balance between the two nerds', (done) => {
+    let nerd1Balance = null
+    next = next.then(() => {
+      return nerd.getBalance()
+    }).then((balance) => {
+      nerd1Balance = balance
+      return nerd2.getBalance()
+    }).then((nerd2Balance) => {
+      log.log('nerd: ' + nerd1Balance + '; nerd2: ' + nerd2Balance)
+      assert(nerd1Balance === nerd2Balance)
+      done()
+    })
+  })
+
+  it('should getBalance with two nerds', (done) => {
+    next = next.then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '190')
+      done()
+    }).catch(handle)
+  })
+
+  it('should send a transfer when there are two nerds', (done) => {
+    next = next.then(() => {
+      return nerd.send({
+        id: 'fifth',
+        account: 'x',
+        amount: '100' 
+      })
+    }).then(() => {
+      // currently, the expectation is that transactions from the nerd side
+      // must be send out through all of the nerds
+      return nerd2.send({
+        id: 'fifth',
+        account: 'x',
+        amount: '100' 
+      })
+    }).then(() => {
+      return Promise.all([
+        new Promise((resolve) => {
+          nerd.once('accept', (transfer, message) => {
+            assert(transfer.id === 'fifth')
+            resolve()
+          })
+        }),
+        new Promise((resolve) => {
+          nerd2.once('accept', (transfer, message) => {
+            assert(transfer.id === 'fifth')
+            resolve()
+          })
+        })
+      ])
+    }).then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '290')
+      done()
+    }).catch(handle)
+  })
+
+  it('should be able to send transfers when one nerd dies', (done) => {
+    next = next.then(() => {
+      return nerd.disconnect()
+    }).then(() => {
+      return noob2.send({
+        id: 'sixth',
+        account: 'x',
+        amount: '40' 
+      })
+    }).then(() => {
+      return new Promise((resolve) => {
+        // the first noob will not receive the accept event for this, because it was
+        // not in the state to receive an acknowledgement
+        noob2.once('accept', (transfer, message) => {
+          assert(transfer.id === 'sixth')
+          resolve()
+        })
+      })
+    }).then(() => {
+      return noob.getBalance()
+    }).then((balance) => {
+      assert(balance === '250')
+      done()
+    }).catch(handle)
+  })
+
   it('should disconnect gracefully', (done) => {
     next.then(() => {
       noob.disconnect()
-      nerd.disconnect()
+      noob2.disconnect()
+      nerd2.disconnect()
       done()
     }).catch(handle)
   }) 

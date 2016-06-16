01c0011262ca6fef64c29d123b6921ce408a30f7
Ben Sharafian <benms64@gmail.com>
2016-06-16 10:13:15 -0700

    refactor: make the channel names less confusing

--- src/model/connection.js
+++ src/model/connection.js
@@ -19,8 +19,9 @@ class Connection extends EventEmitter {
     if (this.config.secret) {
       this.isNoob = false
     }
-    this.recvChannel = this.isNoob + '' + this.token
-    this.sendChannel = !this.isNoob + '' + this.token
+    
+    this.recvChannel = (this.isNoob? 'noob_':'nerd_') + this.token
+    this.sendChannel = (this.isNoob? 'nerd_':'noob_') + this.token
   }
 
   _log (msg) {

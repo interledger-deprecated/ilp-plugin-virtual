ac233b092530d3ac3b5769aa9b865973cc84cf84
Ben Sharafian <benms64@gmail.com>
2017-07-04 16:33:49 +0200

    feat: move payment channel framework code out of plugin virtual

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-'use strict'
+const { MakePaymentChannelPlugin } = require('ilp-plugin-payment-channel-framework')
+const Token = require('./src/token')
+const assert = require('assert')
 
-const Plugin = require('./src/lib/plugin')
-module.exports = Plugin.bind(null, null)
-module.exports.MakePaymentChannelPlugin = (channel) => (Plugin.bind(null, channel))
+module.exports = MakePaymentChannelPlugin({
+  constructor: function (ctx, opts) {
+    assert.equal(typeof opts.secret, 'string', 'opts.secret must be a string')
+    assert.equal(typeof opts.peerPublicKey, 'string', 'opts.peerPublicKey must be a string')
+
+    ctx.state._secret = opts.secret
+    ctx.state._peerAccountName = opts.peerPublicKey
+    ctx.state._accountName = Token.publicKey(ctx.state._secret)
+    ctx.state._prefix = Token.prefix({
+      secretKey: ctx.state._secret,
+      peerPublicKey: ctx.state._peerAccountName,
+      currencyCode: ctx.state._currencyCode,
+      currencyScale: ctx.state._currencyScale
+    })
+
+    ctx.state._info = Object.assign({}, (opts.info || {}), {
+      currencyCode: ctx.state._currencyCode,
+      currencyScale: ctx.state._currencyScale,
+      maxBalance: ctx.state._maxBalance,
+      prefix: ctx.state._prefix
+    })
+
+    ctx.state._account = ctx.state._prefix + ctx.state._accountName
+
+    // Token uses ECDH to get the ledger prefix from secret and public key
+    ctx.state._authToken = Token.authToken({
+      secretKey: ctx.state._secret,
+      peerPublicKey: ctx.state._peerAccountName
+    })
+
+    if (opts.prefix && opts.prefix !== ctx.state._prefix) {
+      throw new Error('invalid prefix. got "' +
+        opts.prefix + '", expected "' + ctx.state._prefix + '"')
+    }
+  },
+
+  getAccount: (ctx) => ctx.state._account,
+  getInfo: (ctx) => ctx.state._info,
+  getAuthToken: (ctx) => ctx.state._authToken,
+
+  connect: async () => {},
+  disconnect: async () => {},
+  handleIncomingPrepare: async () => {},
+  createOutgoingClaim: async () => {},
+  handleIncomingClaim: async () => {}
+})
